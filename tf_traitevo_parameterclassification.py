1acd6839df746bbc57bb210b9643a8869f774949
Liang Xu <xl0418@gmail.com>
2019-01-18 16:56:39 +0100

    remove .xml & tf the parameter classification

--- tf_traitevo_parameterclassification.py
+++ tf_traitevo_parameterclassification.py
@@ -7,102 +7,73 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # print(tf.__version__)
-
-fashion_mnist = keras.datasets.fashion_mnist
-
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-
-
-# # plot the first image
-# plt.figure()
-# plt.imshow(train_images[0])
-# plt.colorbar()
-# plt.grid(False)
-
-# convert the images to grey images
-train_images = train_images / 255.0
-
-test_images = test_images / 255.0
-
-# plt.figure(figsize=(10,10))
-# for i in range(25):
-#     plt.subplot(5,5,i+1)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.grid(False)
-#     plt.imshow(train_images[i], cmap=plt.cm.binary)
-#     plt.xlabel(class_names[train_labels[i]])
-
+# Load the data to train
 file='C:/Liang/Code/Pro2/tf_classification/'
 # # file = '/home/p274981/abcpp/abcpp/'
-filename = file + 'tf_c1.npy'
+filename = file + 'tf_trait2.npy'
 data_train = np.load(filename).item()
+
+# Load the data to test
+file='C:/Liang/Code/Pro2/tf_classification/'
+# # file = '/home/p274981/abcpp/abcpp/'
+filename_test = file + 'tf_c1_test.npy'
+data_test = np.load(filename_test).item()
+
+
+# Normalize the trait data
 Z_train = data_train['Z_train']
 Z_train_normalized = (Z_train-np.min(Z_train))/(np.max(Z_train)-np.min(Z_train))
 
 Labels_train = data_train['Z_labels']
+classnum = len(np.unique(Labels_train))
+#
 model = keras.Sequential([
-    keras.layers.Dense(10, activation=tf.nn.relu),
-    keras.layers.Dense(10, activation=tf.nn.relu),
-    keras.layers.Dense(10, activation=tf.nn.softmax)
+    keras.layers.Dense(64, activation=tf.nn.relu),
+    keras.layers.Dense(64, activation=tf.nn.relu),
+    keras.layers.Dense(classnum, activation=tf.nn.softmax)
 ])
 
 model.compile(optimizer=tf.train.AdamOptimizer(),
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(Z_train_normalized, Labels_train, epochs=5)
+model.fit(Z_train_normalized, Labels_train, epochs=10)
 
-test_loss, test_acc = model.evaluate(test_images, test_labels)
+test_loss, test_acc = model.evaluate(Z_train_normalized, Labels_train)
 
 print('Test accuracy:', test_acc)
 
 # predict for a single image
-predictions = model.predict(test_images)
-predictions[0]
-np.argmax(predictions[0])
-test_labels[0]
+
+# Normalize the test data
+Z_test = data_test['Z_train']
+Z_test_normalized = (Z_test-np.min(Z_test))/(np.max(Z_test)-np.min(Z_test))
+
+Labels_test = data_test['Z_labels']
+
+predictions = model.predict(Z_test_normalized)
+predictions[1]
+for i in range(len(predictions)):
+    print(np.argmax(predictions[i])- Labels_test[i])
 
 
-# plot a batch of predictions
-def plot_image(i, predictions_array, true_label, img):
-  predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
-  plt.grid(False)
-  plt.xticks([])
-  plt.yticks([])
-
-  plt.imshow(img, cmap=plt.cm.binary)
-
-  predicted_label = np.argmax(predictions_array)
-  if predicted_label == true_label:
-    color = 'blue'
-  else:
-    color = 'red'
-
-  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-                                100*np.max(predictions_array),
-                                class_names[true_label]),
-                                color=color)
 
 def plot_value_array(i, predictions_array, true_label):
   predictions_array, true_label = predictions_array[i], true_label[i]
   plt.grid(False)
   plt.xticks([])
   plt.yticks([])
-  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  thisplot = plt.bar(range(6), predictions_array, color="#777777")
   plt.ylim([0, 1])
   predicted_label = np.argmax(predictions_array)
 
   thisplot[predicted_label].set_color('red')
-  thisplot[true_label].set_color('blue')
+  thisplot[int(true_label)].set_color('blue')
 
-i = 0
-plt.figure(figsize=(6, 3))
-plt.subplot(1, 2, 1)
-plot_image(i, predictions, test_labels, test_images)
-plt.subplot(1, 2, 2)
-plot_value_array(i, predictions, test_labels)
+num_rows = 7
+num_cols = 7
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*num_cols, num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, num_cols, i+1)
+  plot_value_array(i, predictions, Labels_test)
